d60f8e8dc330ad01d569923b500e493c1e511b5f
Selena Cardona <cardo23s@mtholyoke.edu>
2016-02-10 15:47:43 -0500

    Update BinaryHeap.java

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -1,14 +1,20 @@
 
 public interface BinaryHeap<T extends Comparable<T>> {
 	
+	
+	// method to add value
 	public void add(T value);
 	
+	//method to remove value from array
 	public T remove();
 	
+	//method to set value to array
 	public T[] toArray(T[] array);
 	
+	//method to get value from array
 	public void fromArray(T[] array);
 	
+	//method that builds maxheap from array
 	public T[] buildMaxHeap(T[] array);
 	
 	
